ff85d8dbfd4956be5363cf08fd28464325f791dd
1f10260159 <s1f102601597@iniad.org>
2026-06-26 13:50:19 +0900

    表紙の写真追加

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ window.addEventListener("DOMContentLoaded", () => {
     "img2.png",
     "img3.png",
     "img4.png",
-    "img5.png"
+    "img5.png",
+    "img6.png",
+    "img7.png",
+    "img8.png",
+    "img9.png",
+    "img10.png",
+    "img11.png",
+    "img12.png",
   ];
 
   const slide = document.getElementById("slideImage");
