cd326d8b0087441091a428d228d4afe10752096b
1f10260159 <s1f102601597@iniad.org>
2026-06-27 18:32:42 +0900

    bgmページ
    bgmのページの修正

--- js/bgm.js
+++ js/bgm.js
@@ -1,6 +1,31 @@
 const player = document.getElementById('player');
+let currentButton = null;
 
-function playMusic(file){
-    player.src = "audio/" + file;
-    player.play();
+function playMusic(button, file) {
+    const audioPath = "audio/" + file; 
+
+    if (!player.src.includes(audioPath)) {
+        player.src = audioPath;
+        player.play();
+        
+        if (currentButton) {
+            currentButton.textContent = '再生';
+            currentButton.classList.remove('playing');
+        }
+        
+        button.textContent = '停止';
+        button.classList.add('playing');
+        currentButton = button;
+    } 
+    else {
+        if (player.paused) {
+            player.play();
+            button.textContent = '停止';
+            button.classList.add('playing');
+        } else {
+            player.pause();
+            button.textContent = '再生';
+            button.classList.remove('playing');
+        }
+    }
 }
